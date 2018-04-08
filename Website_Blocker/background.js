9bd992cd80e9635c9b227aeff28ac99fe4574747
wfan19 <1472451850@qq.com>
2018-04-08 16:22:09 -0400

    Added options

--- Website_Blocker/background.js
+++ Website_Blocker/background.js
@@ -1,5 +1,17 @@
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
+//copied from "Study buddy" Stack Overflow thread
+// https://stackoverflow.com/questions/43889727/how-do-i-block-certain-websites-with-my-chrome-extension
+
+console.log("Loaded extension");
+
+
+function blockRequest(details) {
+   return {cancel: true};
+}
+
+function updateFilters(urls) {
+   if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.facebook.com/*", "*://*.facebook.net/*"]}, ['blocking']);
+}
+
+updateFilters();
