5e4dd1e5f8f717aba85b1963dd7d54ae7b386f5a
wfan19 <1472451850@qq.com>
2018-04-12 08:40:26 -0400

    changed blocking to change HTML through content script

--- Website_Blocker/background.js
+++ Website_Blocker/background.js
@@ -1,10 +1,10 @@
-//copied from "Study buddy" Stack Overflow thread
-// https://stackoverflow.com/questions/43889727/how-do-i-block-certain-websites-with-my-chrome-extension
+  //copied from "Study buddy" Stack Overflow thread
+  // https://stackoverflow.com/questions/43889727/how-do-i-block-certain-websites-with-my-chrome-extension
 
 console.log("Loaded extension");
 
 
-function blockRequest(details) {
+/*function blockRequest(details) {
    return {cancel: true};
 }
 
@@ -12,6 +12,6 @@ function updateFilters(urls) {
    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
      chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.facebook.com/*", "*://*.facebook.net/*"]}, ['blocking']);
-}
+}*/
 
 updateFilters();
