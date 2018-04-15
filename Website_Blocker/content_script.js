c00eb8b66bdc348b83ffabd8f46bf60a33159c21
wfan19 <1472451850@qq.com>
2018-04-15 18:15:11 -0400

    stylized blocking text

--- Website_Blocker/content_script.js
+++ Website_Blocker/content_script.js
@@ -2,5 +2,18 @@ window.addEventListener ("load", myMain, true);
 
 function myMain(evt)
 {
-    document.body.innerHTML = "<h>GO DO YOUR HOMEWORK</h>";
+    document.body.innerHTML = "";
+    var text = document.createElement("h");
+    var tNode  = document.createTextNode("GO WORK"); //text will eventually be customizable
+    text.appendChild(tNode);
+    document.body.appendChild(text);
+    text.style.fontSize = "60px";
+    text.style.fontFamily = "Sans Serif"
 }
+
+/*
+function removeCSS (file)
+{
+    
+}
+*/
