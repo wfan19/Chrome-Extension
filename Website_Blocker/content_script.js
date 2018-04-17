78e7730a076295ba6a7d399e2c2ffd88821d1823
wfan19 <1472451850@qq.com>
2018-04-17 08:42:17 -0400

    Added time setting, and notification is not centered

--- Website_Blocker/content_script.js
+++ Website_Blocker/content_script.js
@@ -1,14 +1,35 @@
 window.addEventListener ("load", myMain, true);
 
+var date = new Date();
+
+console.log(date.getSeconds()+60*date.getMinutes()+3600*date.getHours());
+
 function myMain(evt)
 {
-    document.body.innerHTML = "";
-    var text = document.createElement("h");
-    var tNode  = document.createTextNode("GO WORK"); //text will eventually be customizable
-    text.appendChild(tNode);
-    document.body.appendChild(text);
-    text.style.fontSize = "60px";
-    text.style.fontFamily = "Sans Serif"
+    if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (2 * 3600)
+        && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (23 * 3600))
+    {
+     document.body.innerHTML = "";
+     var text = document.createElement("div");
+     var tNode  = document.createTextNode("GO WORK"); //text will eventually be customizable
+     text.appendChild(tNode);
+     document.body.appendChild(text);
+     text.style.fontSize = "60px";
+     text.style.fontFamily = "Sans Serif"
+     /*
+     text.style.marginLeft = "500px";
+     text.style.marginTop = "250px"
+    */
+
+     text.style.marginTop = "-100px";
+     //text.style.marginLeft = "-50px";
+     text.style.position = "absolute";
+     text.style.textAlign = "center";
+     text.style.top = "50%";
+     text.style.left = "0";
+     text.style.width = "100%";
+     //text.style.height = "100%";
+    }
 }
 
 /*
