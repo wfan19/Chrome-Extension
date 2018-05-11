264a99c00bcc4eb6e839f31aeec519487de2c58d
wfan19 <1472451850@qq.com>
2018-05-11 17:26:48 -0400

    added in dom

--- Website_Blocker/background.js
+++ Website_Blocker/background.js
@@ -15,42 +15,16 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab)
 
 function myMain(tabs)
 {
-  console.log(tabs[0].url);
-  chrome.tabs.executeScript({
-    file:
-      'content_script.js'
-  });
-
-
-
-  /*
-    if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (0 * 3600) //initial time
-            && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (24 * 3600)) //ending time, all times in Millitary time
-    {
-      console.log("test");
-      console.log(tabs);
-        for(var i = 0; i < tabs.length; i++) //loops through all tabs found with youtube URL and changes them.
-        {
-            console.log(i);
-            console.log(tabs[i].url);
-
-            var tab = tabs[i];
-
-            tab.body.innerHTML = "";
-            var text = tab.createElement("div");
-            var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
-            text.appendChild(tNode);
-            tab.body.appendChild(text);
-            text.style.fontSize = "60px";
-            text.style.fontFamily = "Sans Serif"
-
-            text.style.marginTop = "-100px";
-            text.style.position = "absolute";
-            text.style.textAlign = "center";
-            text.style.top = "50%";
-            text.style.left = "0";
-            text.style.width = "100%";
-        }
-    }
-    */
+  if(tabs.length != 0)
+  {
+    console.log(tabs);
+    console.log(tabs[0].url);
+    chrome.tabs.executeScript(tabs[0].id, {
+      file:
+        'content_script.js',
+    });
+    console.log("content script executed..?")
+  }
+  else
+    console.log("No matching tabs");
 }
--- Website_Blocker/content_script.js
+++ Website_Blocker/content_script.js
@@ -1,18 +1,17 @@
 console.log("Content Script Loaded");
 
+var date = new Date();
 
     if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (0 * 3600)
             && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (24 * 3600))
     {
-        var tab = tabs[0];
-        console.log("testing 1234");
+        console.log("About to edit DOM");
 
-        /*
-        tab.body.innerHTML = "";
-        var text = tab.createElement("div");
-        var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
+        document.body.innerHTML = "";
+        var text = document.createElement("div");
+        var tNode  = document.createTextNode("GO WORK"); //text will eventually be customizable
         text.appendChild(tNode);
-        tab.body.appendChild(text);
+        document.body.appendChild(text);
         text.style.fontSize = "60px";
         text.style.fontFamily = "Sans Serif"
 
@@ -22,30 +21,4 @@ console.log("Content Script Loaded");
         text.style.top = "50%";
         text.style.left = "0";
         text.style.width = "100%";
-        */
-
-        /*
-        for(var i = 0; i < tabs.length; i++) //loops through all tabs found with youtube URL and changes them.
-        {
-            console.log(i);
-            console.log(tabs[i].url);
-
-            var tab = tabs[i];
-
-            tab.body.innerHTML = "";
-            var text = tab.createElement("div");
-            var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
-            text.appendChild(tNode);
-            tab.body.appendChild(text);
-            text.style.fontSize = "60px";
-            text.style.fontFamily = "Sans Serif"
-
-            text.style.marginTop = "-100px";
-            text.style.position = "absolute";
-            text.style.textAlign = "center";
-            text.style.top = "50%";
-            text.style.left = "0";
-            text.style.width = "100%";
-        }
-        */
     }
