09301acd4704fdff4ff63080b7478fec9319df48
wfan19 <1472451850@qq.com>
2018-05-11 13:11:58 -0400

    Switched website detection to background.js
    Conent script programmatical injection still needs to be fixed

--- Website_Blocker/background.js
+++ Website_Blocker/background.js
@@ -1,4 +1,56 @@
-  //copied from "Study buddy" Stack Overflow thread
-  // https://stackoverflow.com/questions/43889727/how-do-i-block-certain-websites-with-my-chrome-extension
-
 console.log("Loaded extension");
+
+var date = new Date();
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) 
+{
+  if (changeInfo.status == 'complete' && tab.active) 
+  {
+    var date = new Date();
+    console.log("Page loaded");
+    console.log(date.getSeconds()+60*date.getMinutes()+3600*date.getHours());
+    chrome.tabs.query({url:"*://www.youtube.com/*"}, myMain)
+  }
+})
+
+function myMain(tabs)
+{
+  console.log(tabs[0].url);
+  chrome.tabs.executeScript({
+    file:
+      'content_script.js'
+  });
+
+
+
+  /*
+    if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (0 * 3600) //initial time
+            && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (24 * 3600)) //ending time, all times in Millitary time
+    {
+      console.log("test");
+      console.log(tabs);
+        for(var i = 0; i < tabs.length; i++) //loops through all tabs found with youtube URL and changes them.
+        {
+            console.log(i);
+            console.log(tabs[i].url);
+
+            var tab = tabs[i];
+
+            tab.body.innerHTML = "";
+            var text = tab.createElement("div");
+            var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
+            text.appendChild(tNode);
+            tab.body.appendChild(text);
+            text.style.fontSize = "60px";
+            text.style.fontFamily = "Sans Serif"
+
+            text.style.marginTop = "-100px";
+            text.style.position = "absolute";
+            text.style.textAlign = "center";
+            text.style.top = "50%";
+            text.style.left = "0";
+            text.style.width = "100%";
+        }
+    }
+    */
+}
--- Website_Blocker/content_script.js
+++ Website_Blocker/content_script.js
@@ -1,27 +1,51 @@
-window.addEventListener ("load", myMain, true);
+console.log("Content Script Loaded");
 
-var date = new Date();
 
-console.log(date.getSeconds()+60*date.getMinutes()+3600*date.getHours());
-
-function myMain(evt)
-{
-    if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (2 * 3600)
-        && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (23 * 3600))
+    if((date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) > (0 * 3600)
+            && (date.getSeconds()+60*date.getMinutes()+3600*date.getHours()) < (24 * 3600))
     {
-     document.body.innerHTML = "";
-     var text = document.createElement("div");
-     var tNode  = document.createTextNode("GO WORK"); //text will eventually be customizable
-     text.appendChild(tNode);
-     document.body.appendChild(text);
-     text.style.fontSize = "60px";
-     text.style.fontFamily = "Sans Serif"
+        var tab = tabs[0];
+        console.log("testing 1234");
 
-     text.style.marginTop = "-100px";
-     text.style.position = "absolute";
-     text.style.textAlign = "center";
-     text.style.top = "50%";
-     text.style.left = "0";
-     text.style.width = "100%";
+        /*
+        tab.body.innerHTML = "";
+        var text = tab.createElement("div");
+        var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
+        text.appendChild(tNode);
+        tab.body.appendChild(text);
+        text.style.fontSize = "60px";
+        text.style.fontFamily = "Sans Serif"
+
+        text.style.marginTop = "-100px";
+        text.style.position = "absolute";
+        text.style.textAlign = "center";
+        text.style.top = "50%";
+        text.style.left = "0";
+        text.style.width = "100%";
+        */
+
+        /*
+        for(var i = 0; i < tabs.length; i++) //loops through all tabs found with youtube URL and changes them.
+        {
+            console.log(i);
+            console.log(tabs[i].url);
+
+            var tab = tabs[i];
+
+            tab.body.innerHTML = "";
+            var text = tab.createElement("div");
+            var tNode  = tab.createTextNode("GO WORK"); //text will eventually be customizable
+            text.appendChild(tNode);
+            tab.body.appendChild(text);
+            text.style.fontSize = "60px";
+            text.style.fontFamily = "Sans Serif"
+
+            text.style.marginTop = "-100px";
+            text.style.position = "absolute";
+            text.style.textAlign = "center";
+            text.style.top = "50%";
+            text.style.left = "0";
+            text.style.width = "100%";
+        }
+        */
     }
-}
